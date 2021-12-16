23dc5762dda1cdb1c09a8f475f97e7e49c9e013a
Emedex007 <ivo.salomon@seznam.cz>
2021-12-16 20:15:35 +0100

    add WIP version

--- src/Main.purs
+++ src/Main.purs
@@ -14,7 +14,8 @@ module Main
   where
 
 import Prelude
-import Data.Foldable (any)
+
+import Data.Foldable (any, length)
 import Data.Grid (Coordinates)
 import Data.List (List(..), (:), snoc, last)
 import Data.List as List
@@ -24,24 +25,28 @@ import Data.Set (empty, insert, member) as Set
 import Data.Traversable (for_)
 import Effect (Effect)
 import Effect.Class (liftEffect)
+import Effect.Exception (throwException)
 import Effect.Random (randomInt)
+import Halogen.HTML (elementNS)
 import Reactor (getW, runReactor, updateW_)
 import Reactor.Events (Event(..))
 import Reactor.Graphics.Colors as Color
 import Reactor.Graphics.Drawing (Drawing, fill, tile)
 import Reactor.Reaction (Reaction, executeDefaultBehavior)
+import Web.TouchEvent.EventTypes (touchend)
+import Web.UIEvent.WheelEvent (deltaY)
 
 
 main :: Effect Unit
 main = do
   fx <- liftEffect (randomInt 0 $ widthGlobal - 1)
   fy <- liftEffect (randomInt 0 $ heightGlobal - 1)
-  let initial = { snake: ({ x: 0, y: 0 }: Nil), food: {x: fx, y: fy}, enemy: ({ x: widthGlobal - 1, y: heightGlobal - 1 } : Nil), currentTick: 0, snakeDirection: {dx: 0, dy: 0}}
+  let initial = { snake: ({ x: 0, y: 0 }: Nil), food: {x: fx, y: fy}, enemy: ({ x: widthGlobal - 1, y: heightGlobal - 1 } : Nil), currentTick: 0, snakeDirection: {dx: 0, dy: 0}, enemyDirection: {dx: 0, dy: 0}, counter: 0 }
   let reactor = { initial, draw, handleEvent, isPaused: const false }
   runReactor reactor { title: "Snake", width: widthGlobal, height: heightGlobal }
 
 
-type World = { snake :: List Coordinates, food :: Coordinates, enemy :: List Coordinates, currentTick :: Int, snakeDirection :: {dx :: Int, dy :: Int} }
+type World = { snake :: List Coordinates, food :: Coordinates, enemy :: List Coordinates, currentTick :: Int, snakeDirection :: {dx :: Int, dy :: Int}, enemyDirection :: {dx :: Int, dy :: Int}, counter :: Int }
 widthGlobal :: Int
 widthGlobal = 20
 heightGlobal :: Int
@@ -70,10 +75,10 @@ handleEvent event = do
       {currentTick} <- getW
       updateW_ {currentTick: currentTick + 1}
       if (mod currentTick 30 == 0) && (mod currentTick 20 == 0) then do
-        enemyAI
+        enemyAI Nil 0
         snakeTurn
-      else if (mod currentTick 1 == 0) then do
-        enemyAI
+      else if (mod currentTick 30 == 0) then do
+        enemyAI Nil 0
         snakeDed
         foodEaten
       else if (mod currentTick 20 == 0) then do
@@ -143,29 +148,83 @@ hitWall = do
   {snake, snakeDirection: {dx, dy}} <- getW
   let {x, y} = fromMaybe {x: 0, y: 0} $ List.head snake
   when (x + dx == -1 || x + dx == widthGlobal || y + dy == -1 || y + dy == heightGlobal) 
-    (updateW_ {snake: ({x: 0, y: 0} : Nil), enemy: ({x: widthGlobal - 1, y: heightGlobal - 1} : Nil), snakeDirection: {dx: 0, dy: 0}})
+    (updateW_ {snake: ({x: 0, y: 0} : Nil), enemy: ({x: widthGlobal - 1, y: heightGlobal - 1} : Nil), snakeDirection: {dx: 0, dy: 0}, enemyDirection: {dx: 0, dy: 0}})
 
-enemyAI :: Reaction World
-enemyAI = do
+choseBestWay :: Reaction World
+choseBestWay = do
   {enemy, food} <- getW
-  let newTail = fromMaybe Nil $ List.init enemy
   coin <- liftEffect $ randomInt 0 1
   if coin == 0 then
-    moveEnemyX enemy food newTail
+    moveEnemyX enemy food
   else
-    moveEnemyY enemy food newTail 
+    moveEnemyY enemy food
 
 
-moveEnemyX :: List Coordinates -> Coordinates -> List Coordinates -> Reaction World
-moveEnemyX Nil _ _ = pure unit
-moveEnemyX ({x, y} : _) {x: fx, y: fy} newTail
-  | (fx - x) > 0 = updateW_ {enemy: {x: x + 1, y} : newTail}
-  | (fx - x) < 0 = updateW_ {enemy: {x: x - 1, y} : newTail}
-  | otherwise = moveEnemyY ({x, y} : Nil) {x: fx, y: fy} newTail
+moveEnemyX :: List Coordinates -> Coordinates -> Reaction World
+moveEnemyX Nil _ = pure unit
+moveEnemyX ({x, y} : _) {x: fx, y: fy}
+  | (fx - x) > 0 = updateW_ {enemyDirection: {dx: 1, dy: 0}}
+  | (fx - x) < 0 = updateW_ {enemyDirection: {dx: -1, dy: 0}}
+  | otherwise = moveEnemyY ({x, y} : Nil) {x: fx, y: fy}
 
-moveEnemyY :: List Coordinates -> Coordinates -> List Coordinates -> Reaction World
-moveEnemyY Nil _ _ = pure unit
-moveEnemyY ({x, y} : _) {x: fx, y: fy} newTail 
-  | (fy - y) > 0 = updateW_ {enemy: {x, y: y + 1} : newTail}
-  | (fy - y) < 0 = updateW_ {enemy: {x, y: y - 1} : newTail}
-  | otherwise = (moveEnemyX ({x, y} : Nil) {x: fx, y: fy} newTail)
+moveEnemyY :: List Coordinates -> Coordinates -> Reaction World
+moveEnemyY Nil _ = pure unit
+moveEnemyY ({x, y} : _) {x: fx, y: fy} 
+  | (fy - y) > 0 = updateW_ {enemyDirection: {dx: 0, dy: 1}}
+  | (fy - y) < 0 = updateW_ {enemyDirection: {dx: 0, dy: -1}}
+  | otherwise = moveEnemyX ({x, y} : Nil) {x: fx, y: fy}
+
+choseAlternateDirection :: List {px :: Int, py :: Int} -> Coordinates -> List Coordinates -> Reaction World
+choseAlternateDirection Nil _ _ = pure unit
+choseAlternateDirection ({px, py}:Nil) {x, y} newTail = updateW_ {enemy: {x: x + px, y: y + py}: newTail}
+choseAlternateDirection ({px: px1, py: py1}:{px: px2, py: py2}:_) {x, y} newTail = do
+  coin <- liftEffect $ randomInt 0 1
+  if coin == 0 then
+    updateW_ {enemy: {x: x + px1, y: y + py1}: newTail}
+  else
+    updateW_ {enemy: {x: x + px2, y: y + py2}: newTail}
+
+
+enemyAI :: List {px :: Int, py :: Int} -> Int -> Reaction World
+enemyAI usedDirections acc = do
+  choseBestWay
+  shortWay <- getFastWays Nil
+  let aaab = <$> 
+  {enemy, snake, enemyDirection: {dx, dy}} <- getW
+  let newTail = fromMaybe Nil $ List.init enemy
+  let tailEnemy = fromMaybe Nil $ List.tail enemy
+  let {x, y} = fromMaybe {x: 0, y: 0} $ List.head enemy
+  let possibleDirections = ({px: 1, py: 0}:{px: -1, py: 0}:{px: 0, py: 1}:{px: 0, py: -1}:Nil)
+  let worseDirections = List.difference possibleDirections usedDirections
+
+  if List.length usedDirections < 2  then
+    if any (_ == {x: x + dx, y: y + dy}) (tailEnemy <> snake) then 
+      enemyAI (List.nub $ {px: dx, py: dy} : usedDirections) (acc + 1)
+    else 
+      updateW_ {enemy: {x: x + dx, y: y + dy} : newTail}
+  else
+    choseAlternateDirection worseDirections {x, y} newTail
+ -- Mám dva listy A B. V listu A potřebuju ke každému prvku něco přičíst. Následně potřebuju porovnat každý prvek z A s každým z B. 
+  -- where
+  --   mapfunc {dx, dy} = do
+  --     {enemy, snake} <- getW
+  --     let tailEnemy = fromMaybe Nil $ List.tail enemy
+  --     let {x, y} = fromMaybe {x: 0, y: 0} $ List.head enemy
+  --     if aaa <$> (tailEnemy <> snake) then 
+  --       unit
+  --     else
+  --       {dx, dy}
+  --     where
+  --       aaa ree = 
+  --         if ree == {x: dx + x, y: dy + y} then 
+  --           unit
+  --         else
+  --           {x: dx + x, y: dy + y}
+getFastWays fastWays = do
+  choseBestWay
+  {enemyDirection: {dx, dy} } <- getW
+  if (List.length fastWays) < 2 then
+    getFastWays (List.nub $ {dx, dy} : fastWays)
+  else
+    pure fastWays
+
